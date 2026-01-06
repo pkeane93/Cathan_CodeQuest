d1a44d2bdad44050ad251c71ca30f492fe2a506e
Pierre Keane <pke.webdev@gmail.com>
2026-01-06 11:16:54 +0700

    seeded the project with test posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_01_06_031720) do
+ActiveRecord::Schema[7.1].define(version: 2026_01_06_035717) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.integer "post_id", null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_01_06_031720) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
